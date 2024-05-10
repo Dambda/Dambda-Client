e319f014b818a98998d5f2a41f0ce7481712f0b3
SeHyun <rla6170@naver.com>
2024-05-11 02:28:15 +0900

    Global Style init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
-import React from 'react'
+import './App.css';
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyle';
 
 function App() {
  
   return (
     <>
-      
+      <GlobalStyles />
     </>
   )
 }
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,57 @@
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset};
+
+    @font-face {
+    font-family: 'Pretendard';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+    }
+
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Medium.woff') format('woff');
+        font-weight: 500;
+        font-style: medium;
+    }
+
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-SemiBold.woff') format('woff');
+        font-weight: 600;
+        font-style: semibold;
+    }
+
+    @font-face {
+        font-family: 'Pretendard';
+        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff') format('woff');
+        font-weight: 700;
+        font-style: bold;
+    }
+
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        font-family: 'Pretendard', 'Spline Sans' !important;
+        box-sizing: border-box;
+    }
+    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    a, dl, dt, dd, ol, ul, li, form, label, table{
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 10px;
+        vertical-align: baseline;
+    }
+
+    #root {
+    min-width: 1440px;
+  }
+`;
+
+export default GlobalStyles;
