a8931c3f82b2efc0d5b4494a592eb9456a37a43e
Yean <kjan0929@naver.com>
2024-05-23 18:59:30 +0900

    Add backward button in writing page

--- src/pages/WritingPage.jsx
+++ src/pages/WritingPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import WritingMenu from '../components/writing/WritingMenu';
 import Conatiner from '@/styles/pages/writingpage';
 import WritingInput from '../components/writing/WritingInput';
+import backwardBtn from '@/assets/icon/backwardBtn.svg';
 
 const WritingPage = () => {
   return (
     <Conatiner>
+      <Link to="/">
+        {' '}
+        <img className="backward-btn" src={backwardBtn} />
+      </Link>
       <WritingMenu />
       <WritingInput />
     </Conatiner>
